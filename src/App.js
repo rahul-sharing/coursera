01e23c4215226d69ca37f8d48e572a3b7a47d4da
dipakdas879aa@gamil.com <rahuldhalpaltfg@gmail.com>
2023-08-11 16:18:24 +0530

    added meta tag n semantic elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Footer/>
+    <nav></nav>
+    <main></main>
+    <footer></footer>
       <p>Home Page</p>
     </div>
   );
